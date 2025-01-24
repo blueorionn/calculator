6589c4c89cfbd025e9c1bff37e95b12a6ead7e83
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-24 03:39:57 +0000

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="w-full">
+        <section className="w-full max-w-screen-md mx-auto p-4 flex justify-center items-center">
+          <span className="mx-2 cursor-default text-gray-400">
+            &copy; 2024 Functionbasket.
+          </span>
+        </section>
+      </footer>
+    </>
+  );
+}
