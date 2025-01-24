0e689e82384e51d065b60066983748a3bca5573a
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-24 03:54:19 +0000

    basiadd c calculator logic page

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import BasicCalculator from "@/calculators/basic/BasicCalculator";
 import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
     <>
       <Header />
+      <BasicCalculator />
       <Footer />
     </>
   );
---
+++ src/calculators/basic/BasicCalculator.tsx
@@ -0,0 +1,7 @@
+export default function BasicCalculator() {
+  return (
+    <>
+      <main className="h-full w-full py-8 lg:py-12 xl:py-16"></main>
+    </>
+  );
+}
