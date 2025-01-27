085c612278bcde4f082c91deb9636ced891fb695
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-27 04:11:22 +0000

    add additional types on calculator action

--- src/calculators/basic/SharedTypes.ts
+++ src/calculators/basic/SharedTypes.ts
@@ -6,7 +6,9 @@ export interface CalculatorState {
 
 export type CalculatorAction =
   | { type: "ADD_DIGIT"; payload: string }
+  | { type: "ADD_PERIOD" }
   | { type: "CHOOSE_OPERATION"; payload: string }
   | { type: "CLEAR" }
+  | { type: "CLEAR_ALL" }
   | { type: "DELETE" }
   | { type: "EVALUATE" };
