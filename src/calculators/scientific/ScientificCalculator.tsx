81c7d6ab91139636f405c167ab5ff2f15864603b
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-29 04:27:00 +0000

    add scientific calculator shell ui

--- src/calculators/scientific/ScientificCalculator.tsx
+++ src/calculators/scientific/ScientificCalculator.tsx
@@ -1,6 +1,7 @@
-import { Space_Grotesk } from "next/font/google";
+import { Space_Grotesk, Roboto } from "next/font/google";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+const lato = Roboto({ subsets: ["latin"], weight: "400" });
 
 export default function ScientificCalculator() {
   return (
@@ -23,7 +24,76 @@ export default function ScientificCalculator() {
             </div>
             <div
               className={`${spaceGrotesk.className} grid grid-rows-5 gap-2.5 mt-4`}
-            ></div>
+            >
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">RAD</button>
+                <button type="button">DEG</button>
+                <button type="button">M+</button>
+                <button type="button">M-</button>
+                <button type="button">MC</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">2nd</button>
+                <button type="button">SIN</button>
+                <button type="button">COS</button>
+                <button type="button">TAN</button>
+                <button type="button">MOD</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button" className={`${lato.className}`}>
+                  &pi;
+                </button>
+                <button type="button" className={`${lato.className}`}>
+                  e
+                </button>
+                <button type="button">&radic;x</button>
+                <button type="button">
+                  x<sup>2</sup>
+                </button>
+                <button type="button">
+                  x<sup>3</sup>
+                </button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button" className={`${lato.className}`}>
+                  {"("}
+                </button>
+                <button type="button" className={`${lato.className}`}>
+                  {")"}
+                </button>
+                <button type="button">AC</button>
+                <button type="button">B</button>
+                <button type="button">&divide;</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">1/x</button>
+                <button type="button">7</button>
+                <button type="button">8</button>
+                <button type="button">9</button>
+                <button type="button">&times;</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">n!</button>
+                <button type="button">4</button>
+                <button type="button">5</button>
+                <button type="button">6</button>
+                <button type="button">-</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">ln</button>
+                <button type="button">1</button>
+                <button type="button">2</button>
+                <button type="button">3</button>
+                <button type="button">+</button>
+              </div>
+              <div aria-label="row_one" className="grid grid-cols-5 gap-2.5">
+                <button type="button">lg</button>
+                <button type="button">%</button>
+                <button type="button">0</button>
+                <button type="button">.</button>
+                <button type="button">=</button>
+              </div>
+            </div>
           </section>
         </section>
       </main>
