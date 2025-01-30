b365b9d7bf8b5211a47f7c76d5d4f49718a5d5b0
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-30 02:55:17 +0000

    added types for scientific calculator logic

---
+++ src/calculators/scientific/SharedTypes.ts
@@ -0,0 +1,16 @@
+export interface CalculatorState {
+  currentOperand: string;
+  previousOperand: string;
+  operation: string | null;
+  memory: string;
+  angle: "deg" | "rad";
+}
+
+export type CalculatorActionType =
+  | { type: "ADD_DIGIT"; payload: number }
+  | { type: "ADD_PERIOD" }
+  | { type: "CHOOSE_OPERATION"; payload: string }
+  | { type: "MODULO" }
+  | { type: "CLEAR_ALL" }
+  | { type: "DELETE" }
+  | { type: "EVALUATE" };
