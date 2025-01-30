222b9058539ca0a03d1515b712228c2212c6c657
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-30 03:02:40 +0000

    add reducer function

---
+++ src/calculators/scientific/CalculatorLogic.ts
@@ -0,0 +1,27 @@
+import { CalculatorState, CalculatorActionType } from "./SharedTypes";
+
+export const INITIAL_STATE: CalculatorState = {
+  currentOperand: "0",
+  previousOperand: "0",
+  operation: null,
+  memory: "0",
+  angle: "deg",
+};
+
+export function reducer(
+  state: CalculatorState,
+  action: CalculatorActionType
+): CalculatorState {
+  switch (action.type) {
+    case "ADD_DIGIT":
+      if (state.currentOperand === "0")
+        return { ...state, currentOperand: `${action.payload}` };
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand}${action.payload}`,
+      };
+
+    default:
+      return state;
+  }
+}
