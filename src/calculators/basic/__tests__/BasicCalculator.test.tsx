ea986a16729d5a0de843fe8e47646dc4d9e0d12e
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-02-05 19:34:34 +0000

    added empty tests for button visibility

--- src/calculators/basic/__tests__/BasicCalculator.test.tsx
+++ src/calculators/basic/__tests__/BasicCalculator.test.tsx
@@ -2,10 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import BasicCalculator from "../BasicCalculator";
 
+describe("Test for component visibility in document", () => {
+  it("Test for digit buttons visibility", () => {
+    render(<BasicCalculator />);
+  });
+});
+
 describe("Test Operations", () => {
   it("Test addition operation", () => {
     render(<BasicCalculator />);
 
-    expect(screen.getByText("=")).toBeInTheDocument();
+    expect(screen.getByLabelText("1")).toBeInTheDocument();
   });
 });
