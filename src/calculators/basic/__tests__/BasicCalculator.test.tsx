cfe9a695b0b3b8eaefdec723aa70c40b5238bc13
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-02-05 19:46:23 +0000

    added tests for buttons visibility

--- src/calculators/basic/__tests__/BasicCalculator.test.tsx
+++ src/calculators/basic/__tests__/BasicCalculator.test.tsx
@@ -5,13 +5,31 @@ import BasicCalculator from "../BasicCalculator";
 describe("Test for component visibility in document", () => {
   it("Test for digit buttons visibility", () => {
     render(<BasicCalculator />);
+
+    expect(screen.getByTestId("0")).toBeInTheDocument();
+    expect(screen.getByTestId("1")).toBeInTheDocument();
+    expect(screen.getByTestId("2")).toBeInTheDocument();
+    expect(screen.getByTestId("3")).toBeInTheDocument();
+    expect(screen.getByTestId("4")).toBeInTheDocument();
+    expect(screen.getByTestId("5")).toBeInTheDocument();
+    expect(screen.getByTestId("6")).toBeInTheDocument();
+    expect(screen.getByTestId("7")).toBeInTheDocument();
+    expect(screen.getByTestId("8")).toBeInTheDocument();
+    expect(screen.getByTestId("9")).toBeInTheDocument();
+  });
+
+  it("Test for special buttons visibility", () => {
+    render(<BasicCalculator />);
+
+    expect(screen.getByTestId("clear")).toBeInTheDocument();
+    expect(screen.getByTestId("clear-all")).toBeInTheDocument();
+    expect(screen.getByTestId("delete")).toBeInTheDocument();
+    expect(screen.getByTestId("eval")).toBeInTheDocument();
   });
 });
 
 describe("Test Operations", () => {
   it("Test addition operation", () => {
     render(<BasicCalculator />);
-
-    expect(screen.getByLabelText("1")).toBeInTheDocument();
   });
 });
